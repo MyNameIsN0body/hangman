e51930b6f2c88b403655223045058e10ca788204
GooDkov <a.gudkov@ec-rs.ru>
2025-11-04 01:06:11 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String intro = "logo";
+        String gameover = "gameover";
+        printResult(intro);
+    }
+
+    public static void printResult(String str) {
+        String fileName = "C:\\Users\\a.gudkov\\Projects\\Free\\java\\gallows\\gallows\\src\\output.txt";
+        String start = str + "_start";
+        String end = str + "_end";
+        StringBuilder content = new StringBuilder();
+        boolean insideBlock = false;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.equals(start)) {
+                    insideBlock = true;
+                    continue;
+                }
+                if (line.contains(end)) {
+                    insideBlock = false;
+                    break;
+                }
+                if (insideBlock) {
+                    content.append(line).append("\n");
+                }
+
+            }
+
+            System.out.println(content.toString());
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения файла: " + e.getMessage());
         }
+
     }
 }
